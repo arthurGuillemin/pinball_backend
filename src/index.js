dfeb87e4c5c1b034e08c1892474c753096d1feb7
Arthur Guillemin <aguillemin291@gmail.com>
2026-02-18 14:02:21 +0100

    Feat : create routes for top 10 high scores and add a new score

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import express from 'express'
 import { createServer } from 'http'
 import { Server } from 'socket.io'
 import dotenv from 'dotenv'
+import scoresRouter from './routes/scores.route.js'
+
 
 dotenv.config()
 
+const PORT = process.env.PORT || 3000
 const app = express()
 const httpServer = createServer(app)
+
 const io = new Server(httpServer, {
   cors: {
     origin: '*'
@@ -15,13 +19,14 @@ const io = new Server(httpServer, {
 
 app.use(express.json())
 
-const PORT = process.env.PORT || 3000
 
 
 // test route
 app.get('/', (req, res) => {
   res.json({ status: 'server is on' })
 })
+//scores routes 
+app.use('/api/scores', scoresRouter)
 
 httpServer.listen(PORT, () => {
   console.log(`Serveur lancé sur le port ${PORT}`)
