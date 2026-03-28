ca6d754c9c616e9ae59579cc2d809cfe35640adf
ArthurGuillemin <aguillemin291@gmail.com>
2026-03-28 18:29:17 +0100

    fix(api): improve error handling and validation flow

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,9 +1,27 @@
 const errorHandler = (err, req, res, next) => {
   console.error(err);
-  const statusCode = err.statusCode || 500;
-  res.status(statusCode).json({
-    status: err.status || "error",
-    message: err.message || "Internal Server Error",
+  // erreru zod
+  if (err.name === "ZodError") {
+    return res.status(400).json({
+      status: "fail",
+      message: "data validation error",
+      errors: err.issues.map((e) => ({
+        field: e.path.join("."),
+        message: e.message,
+      })),
+    });
+  }
+  // erreur custom
+  if (err.statusCode) {
+    return res.status(err.statusCode).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+  // erreur inconnue
+  return res.status(500).json({
+    status: "error",
+    message: "Internal Server Error",
   });
 };
 
--- src/utils/appError.js
+++ src/utils/appError.js
@@ -2,6 +2,7 @@ class AppError extends Error {
   constructor(message, statusCode) {
     super(message);
     this.statusCode = statusCode;
+    this.status = "fail";
     Error.captureStackTrace(this, this.constructor);
   }
 }
