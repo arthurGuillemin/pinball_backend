f172686d7a37c8546d1e254f32f0d3571bdc9ba7
ArthurGuillemin <aguillemin291@gmail.com>
2026-03-30 10:49:12 +0200

    Feat(middleware) : configure rate limiter on POST route

--- src/routes/scores.route.js
+++ src/routes/scores.route.js
@@ -5,12 +5,13 @@ import {
   getLeaderboard,
   addNewScore,
 } from "../controllers/score.controller.js";
+import postScoreLimiter from "../middlewares/rateLimit.js";
 const router = Router();
 
 // GET /api/scores - top 10
 router.get("/", getLeaderboard);
 
 // POST /api/scores - add score
-router.post("/", validate(scoreSchema), addNewScore);
+router.post("/", postScoreLimiter, validate(scoreSchema), addNewScore);
 
 export default router;
