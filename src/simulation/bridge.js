f74904b4ae666c4bd20574b2330885f3afddc0a5
Younes <younes.benaggoun3@gmail.com>
2026-04-06 14:19:16 +0100

    simulation

--- src/simulation/bridge.js
+++ src/simulation/bridge.js
@@ -1,7 +1,7 @@
 import { WebSocketServer } from 'ws';
 import mqtt from "mqtt"; // import namespace "mqtt"
 
-let client = mqtt.connect("mqtt://captain.dev0.pandor.cloud:1884"); // create a client
+var client = mqtt.connect("mqtt://captain.dev0.pandor.cloud:1884"); // create a client
 
 
 const topic = "classroom/Younes";
@@ -13,6 +13,8 @@ client.on("connect", () => {
         }
     });
 });
+
+
 
 
 
